9d47b9944b48575d1ecde991815e428754154e32
itsMeDiksha <dikshakumarione19@gmail.com>
2025-05-21 01:39:14 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-})
+    base: import.meta.env.VITE_BASE_PATH || "rect-vite-ToDo"
+    // Use import.meta.env for Vite environment variables
+    // base: import.meta.env.VITE_BASE_PATH || "/"
+  })
